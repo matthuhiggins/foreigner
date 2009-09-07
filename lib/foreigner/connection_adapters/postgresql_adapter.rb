3f4d450e8e03ce97c1914d15d1c144af940420be
Matthew Higgins <matthuhiggins@unicorn.local>
2009-09-06 22:48:03 -0700

    shorten query with using shorthand

--- lib/foreigner/connection_adapters/postgresql_adapter.rb
+++ lib/foreigner/connection_adapters/postgresql_adapter.rb
@@ -6,8 +6,6 @@ module Foreigner
       include Foreigner::ConnectionAdapters::Sql2003
       
       def foreign_keys(table_name)
-        foreign_keys = []
-
         fk_info = select_all %{
           select tc.constraint_name as name
                 ,ccu.table_name as to_table
@@ -15,17 +13,11 @@ module Foreigner
                 ,rc.delete_rule as dependency
           from information_schema.table_constraints tc
           join information_schema.key_column_usage kcu
-            on tc.constraint_catalog = kcu.constraint_catalog
-           and tc.constraint_schema = kcu.constraint_schema
-           and tc.constraint_name = kcu.constraint_name
+          using (constraint_catalog, constraint_schema, constraint_name)
           join information_schema.referential_constraints rc
-            on tc.constraint_catalog = rc.constraint_catalog
-           and tc.constraint_schema = rc.constraint_schema
-           and tc.constraint_name = rc.constraint_name
+          using (constraint_catalog, constraint_schema, constraint_name)
           join information_schema.constraint_column_usage ccu
-            on tc.constraint_catalog = ccu.constraint_catalog
-           and tc.constraint_schema = ccu.constraint_schema
-           and tc.constraint_name = ccu.constraint_name
+          using (constraint_catalog, constraint_schema, constraint_name)
           where tc.constraint_type = 'FOREIGN KEY'
             and tc.constraint_catalog = '#{@config[:database]}'
             and tc.table_name = '#{table_name}'
