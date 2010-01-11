00c6e90c69623ceeb7f646ea09edeb4272428210
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-01-11 15:21:10 -0800

    Add support for active_record.table_name_prefix and active_record.table_name_suffix. Thanks accy.

--- lib/foreigner/connection_adapters/sql_2003.rb
+++ lib/foreigner/connection_adapters/sql_2003.rb
@@ -8,23 +8,18 @@ module Foreigner
       def add_foreign_key(from_table, to_table, options = {})
         column  = options[:column] || "#{to_table.to_s.singularize}_id"
         foreign_key_name = foreign_key_name(from_table, column, options)
+        primary_key = options[:primary_key] || "id"
+        dependency = dependency_sql(options[:dependent])
 
         sql =
           "ALTER TABLE #{quote_table_name(from_table)} " +
           "ADD CONSTRAINT #{quote_column_name(foreign_key_name)} " +
-          foreign_key_definition(to_table, options)
+          "FOREIGN KEY (#{quote_column_name(column)}) " +
+          "REFERENCES #{quote_table_name(ActiveRecord::Migrator.proper_table_name(to_table))}(#{primary_key})"
+
+        sql << " #{dependency}" unless dependency.blank?
       
         execute(sql)
-      end
-    
-      def foreign_key_definition(to_table, options = {})
-        column  = options[:column] || "#{to_table.to_s.singularize}_id"
-        primary_key = options[:primary_key] || "id"
-        dependency = dependency_sql(options[:dependent])
-
-        sql = "FOREIGN KEY (#{quote_column_name(column)}) REFERENCES #{quote_table_name(to_table)}(#{primary_key})"
-        sql << " #{dependency}" unless dependency.blank?
-        sql
       end
 
       private
