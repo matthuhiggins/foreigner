9bdfbb78c636d43557cad1cd6a44b7ef9f202b17
Matthew Higgins <developer@matthewhiggins.com>
2011-04-16 12:08:34 -0700

    use straight up joins

--- lib/foreigner/connection_adapters/postgresql_adapter.rb
+++ lib/foreigner/connection_adapters/postgresql_adapter.rb
@@ -7,10 +7,10 @@ module Foreigner
         fk_info = select_all %{
           SELECT t2.relname AS to_table, a1.attname AS column, a2.attname AS primary_key, c.conname AS name, c.confdeltype AS dependency
           FROM pg_constraint c
-          LEFT JOIN pg_class t1 ON c.conrelid = t1.oid
-          LEFT JOIN pg_class t2 ON c.confrelid = t2.oid
-          LEFT JOIN pg_attribute a1 ON a1.attnum = c.conkey[1] AND a1.attrelid = t1.oid
-          LEFT JOIN pg_attribute a2 ON a2.attnum = c.confkey[1] AND a2.attrelid = t2.oid
+          JOIN pg_class t1 ON c.conrelid = t1.oid
+          JOIN pg_class t2 ON c.confrelid = t2.oid
+          JOIN pg_attribute a1 ON a1.attnum = c.conkey[1] AND a1.attrelid = t1.oid
+          JOIN pg_attribute a2 ON a2.attnum = c.confkey[1] AND a2.attrelid = t2.oid
           WHERE c.contype = 'f' AND t1.relname = '#{table_name}'
           ORDER BY c.conname
         }
