f8a714eb9302481fe4fad3eb576a6d9a936b2a12
Jeremy Walker <jez.walker@gmail.com>
2011-05-18 15:22:19 +0100

    Fix issue in recorder with *args

--- test/reversible_test.rb
+++ test/reversible_test.rb
@@ -8,29 +8,33 @@ class ReversibleTest < Foreigner::AdapterTest
   
   test 'invert_remove_foreign_key' do
     recorder = ActiveRecord::Migration::CommandRecorder.new
+    recorded = recorder.add_foreign_key(:table1, :table2)[0]
     assert_equal(
-      send(*recorder.add_foreign_key(:table1, :table2)[0]),
+      send(recorded[0], *recorded[1]),
       send(*recorder.invert_remove_foreign_key(:table1, :table2))
     )
   end
   test 'invert_remove_foreign_key with column' do
     recorder = ActiveRecord::Migration::CommandRecorder.new
+    recorded = recorder.add_foreign_key(:table1, :table2, :column => :column1)[0]
     assert_equal(
-      send(*recorder.add_foreign_key(:table1, :table2, :column => :column1)[0]),
+      send(recorded[0], *recorded[1]),
       send(*recorder.invert_remove_foreign_key(:table1, :table2, :column => :column1))
     )
   end
   test 'invert_add_foreign_key' do
     recorder = ActiveRecord::Migration::CommandRecorder.new
+    recorded = recorder.remove_foreign_key(:table1, :table2)[0]
     assert_equal(
-      send(*recorder.remove_foreign_key(:table1, :table2)[0]),
+      send(recorded[0], *recorded[1]),
       send(*recorder.invert_add_foreign_key(:table1, :table2))
     )
   end
   test 'invert_add_foreign_key with column' do
     recorder = ActiveRecord::Migration::CommandRecorder.new
+    recorded = recorder.remove_foreign_key(:table1, :table2, :column => :column1)[0]
     assert_equal(
-      send(*recorder.remove_foreign_key(:table1, :table2, :column => :column1)[0]),
+      send(recorded[0], *recorded[1]),
       send(*recorder.invert_add_foreign_key(:table1, :table2, :column => :column1))
     )
   end
