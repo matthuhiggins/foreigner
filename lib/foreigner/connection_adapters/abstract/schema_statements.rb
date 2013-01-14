eeab16db589376561c5d99b548b2031aa3c36d27
Matthew Higgins <developer@matthewhiggins.com>
2013-01-13 18:01:26 -0800

    fix up create_table override

--- lib/foreigner/connection_adapters/abstract/schema_statements.rb
+++ lib/foreigner/connection_adapters/abstract/schema_statements.rb
@@ -11,8 +11,11 @@ module Foreigner
     module AbstractAdapter
       def create_table(table_name, *args, &block)
         definition = nil
-        super { |td| definition = td } # This is my trick to get the definition
-        td.foreign_keys.each { |c,o| add_foreign_key table_name, c, o }
+        super do |td|
+          definition = td # This is my trick to get the definition
+          block.call(td)
+        end
+        definition.foreign_keys.each { |c,o| add_foreign_key table_name, c, o }
       end
 
       def supports_foreign_keys?
--- test/foreigner/connection_adapters/abstract/schema_statements_test.rb
+++ test/foreigner/connection_adapters/abstract/schema_statements_test.rb
@@ -1,5 +1,5 @@
 require 'helper'
 
 class Foreigner::ConnectionAdapters::SchemaStatementsTest < ActiveSupport::TestCase
-  
+
 end
