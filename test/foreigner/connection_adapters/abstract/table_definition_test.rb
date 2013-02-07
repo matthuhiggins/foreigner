3857bc6f551c0939d27e4ef5189b0cab9209c68e
David James <david@bluemontlabs.com>
2013-02-06 23:46:46 -0500

    add failing test
    (Note: the first argument of assert_equal is the expected value.)

--- test/foreigner/connection_adapters/abstract/table_definition_test.rb
+++ test/foreigner/connection_adapters/abstract/table_definition_test.rb
@@ -5,9 +5,17 @@ class Foreigner::ConnectionAdapters::TableDefinitionsTest < ActiveSupport::TestC
     include Foreigner::ConnectionAdapters::TableDefinition
   end
 
-  test "foreign_key" do
+  test "foreign_key used once" do
     definition = TestDefinition.new
     definition.foreign_key :poops, and: :one;
-    assert_equal definition.foreign_keys[:poops], and: :one
+    assert_equal({ and: :one }, definition.foreign_keys[:poops])
+  end
+
+  test "foreign_key used twice" do
+    definition = TestDefinition.new
+    definition.foreign_key :nodes, column: :from_id
+    definition.foreign_key :nodes, column: :to_id
+    assert_equal [{ column: :from_id }, { column: :to_id }],
+      definition.foreign_keys[:nodes]
   end
 end
